903ba2ecf3fb771bfad52160b3be420e53a3e707
Anastasiia Vynokurova <anastasi.vinokurova@gmail.com>
2023-03-14 14:09:29 -0500

    something I did last week

---
+++ src/components/lightbulb/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+
+function LihgtBulb(props){
+	const img1 = (props.lightIsOn) ? 'light_on.png' : 'light_off.jpg'
+	return( 
+		<>
+  			<img className="lightbulb" src={img1}/>
+		</>
+	)
+}
+export default LihgtBulb
